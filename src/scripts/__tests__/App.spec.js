56321aa14293a8cd0dfd6398134c7c0cc0d98b92
Jon McPartland <jon@bigbitecreative.com>
2018-12-06 20:09:37 +0000

    Resolve JS linting errors

--- src/scripts/__tests__/App.spec.js
+++ src/scripts/__tests__/App.spec.js
@@ -1,15 +1,7 @@
 import App from '../App';
-import Attribution from '../modules/Attribution';
-
-// Mock the module calls.
-jest.mock('../modules/Attribution', () => jest.fn());
 
 describe('App', () => {
   beforeAll(() => {
     App();
-  });
-
-  test('Attribution has been called', () => {
-    expect(Attribution.mock.calls.length).toBe(1);
   });
 });
