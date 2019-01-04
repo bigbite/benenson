05ed6e1bfddf1e07f7ba8f535c972e7fa3874f07
Scott Blackburn <scott@bigbitecreative.com>
2019-01-04 11:12:49 +0000

    Fix the post grid custom option and add in styling for the feature image

--- src/scripts/blocks/post-list/components/display/PostItem.js
+++ src/scripts/blocks/post-list/components/display/PostItem.js
@@ -1,5 +1,9 @@
 
 const PostItem = props => (<article className="postGrid-item">
+  <figure className="postGrid-item-image" style={ {
+    backgroundImage: `url(${props.featured_image})`,
+  } }>
+  </figure>
   <h3 className="grid-itemTitle"><a>{ props.title }</a></h3>
   { props.tag && <span className="grid-itemMeta"><a>{ props.tag.title }</a></span> }
   { props.excerpt && <div className="grid-itemContent" dangerouslySetInnerHTML={ { __html: props.excerpt } } /> }
