489f789698d4c6e46d35d84b6499d4c66f88fad2
Jon McPartland <jon@bigbitecreative.com>
2019-01-14 11:54:07 +0000

    Improve bump script

--- bin/bump.sh
+++ bin/bump.sh
@@ -1,19 +1,38 @@
 #!/usr/bin/env sh
 
 # escape a version number for sed
-v() {
-    echo $(cat "$1" | tr -d '\n' | sed 's/\./\\\./g')
+_v() {
+    echo $(echo "$1" | tr -d '\n' | sed 's/\./\\\./g');
 }
 
-# X-compatible sed in-place
-sedi() {
-    sed --version > /dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@"
+# cross-compatible sed in-place
+_sedi() {
+    sed --version > /dev/null 2>&1 && sed -i -- "$@" || sed -i "" "$@";
 }
 
-# set old version & bump new version
-cat .versionnew > .versionold
-cat .versionold | awk -F. '/[0-9]+\./{$NF+=1;OFS=".";print}' > .versionnew
+_bump() {
+    local bumptype="${1:-patch}"
+    local oldversion;
+    local newversion;
 
-# find/replace old/new versions in required files
-sedi "s/$(v .versionold)/$(v .versionnew)/g" gulp/tasks/styles.js
-sedi "s/$(v .versionold)/$(v .versionnew)/g" includes/scripts-and-styles.php
+    # retrieve old version
+    oldversion=$(cat "$PWD/gulp/tasks/styles.js" | grep '^\tVersion: ' | awk '{print $2}');
+
+    # bump it
+    case "$bumptype" in
+        major) newversion=$(echo "$oldversion" | awk '{split($NF,v,/[.]/); $NF=++v[1]"."v[2]"."v[3]}1');;
+        minor) newversion=$(echo "$oldversion" | awk '{split($NF,v,/[.]/); $NF=v[1]"."++v[2]"."v[3]}1');;
+        patch) newversion=$(echo "$oldversion" | awk '{split($NF,v,/[.]/); $NF=v[1]"."v[2]"."++v[3]}1');;
+        *)     newversion=$(echo "$oldversion" | awk '{split($NF,v,/[.]/); $NF=v[1]"."v[2]"."++v[3]}1');;
+    esac
+
+    # escape versions for use in sed
+    oldversion=$(_v "$oldversion")
+    newversion=$(_v "$newversion")
+
+    # find/replace old/new versions in required files
+    _sedi "s/$oldversion/$newversion/g" "$PWD/gulp/tasks/styles.js";
+    _sedi "s/$oldversion/$newversion/g" "$PWD/includes/scripts-and-styles.php";
+}
+
+_bump "$1"
